1aaf873ff566b2d82dad349dee29a3fb7ea42a04
rperez23 <ronnie.lperez@gmail.com>
2024-02-14 14:20:53 -0800

    v003

--- changetc.pl
+++ changetc.pl
@@ -1,72 +1,43 @@
-#! /usr/bin/perl
-
-use Getopt::Long;
-
-%captionhash;
+#!/usr/bin/perl
 
 
-GetOptions(
-           'capfile=s'   => \my $capf,
-           'srchr=i'     => \my $srchr,
-           'outhr=i'     => \my $outhr,);
-
-
-if ( ($capf eq "") or ($srchr eq "") or ($outhr eq "") )
-{
-  print("   ~~~ Undefined input $capf : $srchr : $outhr   ~~~\n");
-  exit(1);
-}
-
-$srchr = sprintf("%02d",$srchr);
-$outhr = sprintf("%02d",$outhr);
-
-
-unless (open INF,"<$capf")
-{
-  print("   ~~~Cannot open input file $inf~~~\n");
-  exit(1);
-}
-
-$counter = 0;
-while(<INF>)
-{
-  chomp;
-  $counter += 1;
-
-  #add text to hash, counter is the key
-  $captionhash{$counter} = $_;
-}
-close(INF);
-
-foreach $n (1..$counter)
+foreach $inf (@ARGV)
 {
 
-  $txt = $captionhash{$n};
 
-  #if the text starts with a TC, we have a caption entry
-  if ($txt =~ /^(\d{2})(:\d{2}:\d{2}.+)$/)
+  $outf = "01/$inf";
+  #$outf =~ s/.scc/_ENG.scc/;
+  print("$inf -> $outf\n");
+
+
+  unless (open INF, "<$inf")
   {
-      $hr      = $1;
-      $therest = $2;
-
-      if ($hr eq $srchr)
-      {
-        print("${outhr}${therest}\n");
-      }
-
-      #this is for troubleshooting
-=pod
-      else
-      {
-        print("IGNORING : $txt\n");
-      }
-=cut
-
-
+    print("Cannot Open input file $inf\n");
+    exit(1);
   }
-  else #if not a TC just print it
+
+  unless (open OUTF, ">$outf")
   {
-    print("$txt\n");
+    print("Cannot open output file $outf\n");
+    exit(1);
   }
+
+  while(<INF>)
+  {
+    chomp;
+
+    $txt = $_;
+
+    if (/^00/)
+    {
+      next;
+    }
+
+    #$txt =~ s/^01:/00:/;
+    print OUTF ("$txt\n");
+  }
+
+  close(INF);
+  close(OUTF);
 
 }
