5bbd63fdc91aea2bd784b50b2d28d28363177f9a
rperez23 <ronnie.lperez@gmail.com>
2024-02-15 16:49:06 -0800

    v001

---
+++ changetc1to0.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+
+#1 -> 0
+
+foreach $inf (@ARGV)
+{
+
+
+  $outf = "00/$inf";
+  #$outf =~ s/.scc/_ENG.scc/;
+  print("$inf -> $outf\n");
+
+
+  unless (open INF, "<$inf")
+  {
+    print("Cannot Open input file $inf\n");
+    exit(1);
+  }
+
+  unless (open OUTF, ">$outf")
+  {
+    print("Cannot open output file $outf\n");
+    exit(1);
+  }
+
+  while(<INF>)
+  {
+    chomp;
+
+    $txt = $_;
+
+    if (/^01/)
+    {
+      $txt =~ s/^01:/00:/;
+    }
+
+    print OUTF ("$txt\n");
+  }
+
+  close(INF);
+  close(OUTF);
+
+}
